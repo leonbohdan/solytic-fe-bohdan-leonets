73ec8ca60683a4f635b99609a88e4f3a9b0623a9
bohdan leonets <bohdan@mytowerapp.com>
2023-05-07 23:44:17 +0300

    add some unit test

--- src/components/UserLoginDialog.vue
+++ src/components/UserLoginDialog.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useLoginStore } from "@/stores/useLogin";
 import BaseDialog from "@/components/base/BaseDialog.vue";
 
--- src/components/__tests__/LoginFormView.spec.ts
+++ src/components/__tests__/LoginFormView.spec.ts
@@ -1,11 +1,32 @@
-import { describe, it, expect } from "vitest";
-
+import { describe, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
-import LoginView from "@/views/LoginFormView.vue";
+import { createPinia } from "pinia/dist/pinia";
+import { useLoginStore } from "@/stores/useLogin";
 
-describe("HelloWorld", () => {
-  it("renders properly", () => {
-    const wrapper = mount(LoginView, { props: { msg: "Hello Vitest" } });
-    expect(wrapper.text()).toContain("Hello Vitest");
+import LoginFormView from "@/views/LoginFormView.vue";
+
+describe("LoginFormView", () => {
+  let wrapper: any;
+
+  const setPasswordTo = (password: string | number) => {
+    return wrapper.get('[data-test="password-field"]').setValue(password);
+  };
+
+  beforeEach(() => {
+    wrapper = mount(LoginFormView, {
+      global: {
+        plugins: [createPinia()]
+      }
+    });
+    useLoginStore();
   });
+
+  it("should render", () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  // it("renders properly", () => {
+  //   const wrapper = mount(LoginFormView, { props: { msg: "Hello Vitest" } });
+  //   expect(wrapper.text()).toContain("Hello Vitest");
+  // });
 });
